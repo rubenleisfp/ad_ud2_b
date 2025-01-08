2a7cd5f0aae327364f164460a8d1630a238d2400
Ruben Leis <rubenleisfp@gmail.com>
2025-01-08 10:43:04 +0100

    Dto hecho

---
+++ src/main/java/ad/ad_ud2_b/ejemplo_dto/App.java
@@ -0,0 +1,41 @@
+package ad.ad_ud2_b.ejemplo_dto;
+
+import ad.ad_ud2_b.ejemplo_dto.repository.FilmDao;
+import ad.ad_ud2_b.ejemplo_dto.repository.impl.FilmJdbcDao;
+import ad.ad_ud2_b.ejemplo_dto.service.FilmService;
+
+public class App {
+	
+    FilmService filmService;
+	FilmDao filmDao;
+    
+    public static void main(String[] args) {
+		App app = new App();
+		app.run();
+	}
+    
+    public void run() {
+ 		cfg();
+        getJson();
+    }
+
+    /**
+     * Instanciamos el DAO y la clase SERVICE
+     */
+    private void cfg() {
+        throw new UnsupportedOperationException("Falta implementar por el alumno");
+    }
+
+    /**
+     * LLamamos al service para obtener la informacion en JSON
+     */
+    private void getJson() {
+        try {
+            String json = filmService.getFilmTitleAndYearAsJson();
+            System.out.println("JSON:");
+            System.out.println(json);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
