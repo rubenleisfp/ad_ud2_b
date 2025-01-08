324c7f9fd0aa9728adf2141a98b333022a2fe007
Ruben Leis <rubenleisfp@gmail.com>
2025-01-08 10:43:04 +0100

    Dto hecho
    (cherry picked from commit 2a7cd5f0aae327364f164460a8d1630a238d2400)

---
+++ src/main/java/ad/ad_ud2_b/ejemplo_dto/repository/model/Film.java
@@ -0,0 +1,166 @@
+package ad.ad_ud2_b.ejemplo_dto.repository.model;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Set;
+
+public class Film implements Serializable{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int filmId;
+    private String title;
+    private String description;
+    private short releaseYear;
+    private int languageId;
+    private Integer originalLanguageId;
+    private int rentalDuration;
+    private BigDecimal rentalRate;
+    private Integer length;
+    private BigDecimal replacementCost;
+    private String rating;
+    private Set<String> specialFeatures;
+    private Timestamp lastUpdate;
+
+    // Constructores (pueden incluirse más según sea necesario)
+
+    public Film() {
+    }
+
+    public Film(int filmId, String title, String description, short releaseYear, int languageId,
+                Integer originalLanguageId, int rentalDuration, BigDecimal rentalRate, Integer length,
+                BigDecimal replacementCost, String rating, Set<String> specialFeatures, Timestamp lastUpdate) {
+        this.filmId = filmId;
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.languageId = languageId;
+        this.originalLanguageId = originalLanguageId;
+        this.rentalDuration = rentalDuration;
+        this.rentalRate = rentalRate;
+        this.length = length;
+        this.replacementCost = replacementCost;
+        this.rating = rating;
+        this.specialFeatures = specialFeatures;
+        this.lastUpdate = lastUpdate;
+    }
+
+    // Getters y Setters (pueden incluirse más según sea necesario)
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public short getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(short releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(int languageId) {
+        this.languageId = languageId;
+    }
+
+    public Integer getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(Integer originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public int getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(int rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public BigDecimal getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(BigDecimal rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public BigDecimal getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(BigDecimal replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Set<String> getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(Set<String> specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+	@Override
+	public String toString() {
+		return "Film [filmId=" + filmId + ", title=" + title + ", description=" + description + ", releaseYear="
+				+ releaseYear + ", languageId=" + languageId + ", originalLanguageId=" + originalLanguageId
+				+ ", rentalDuration=" + rentalDuration + ", rentalRate=" + rentalRate + ", length=" + length
+				+ ", replacementCost=" + replacementCost + ", rating=" + rating + ", specialFeatures=" + specialFeatures
+				+ ", lastUpdate=" + lastUpdate + "]";
+	}
+
+    // Otros métodos según sea necesario
+}
