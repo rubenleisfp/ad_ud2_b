6a50a8f967a668d53ed7516a55c12b5a80315fdf
rubenleisfp <rubenleisfp@gmail.com>
2025-05-22 10:16:47 +0200

    Agregamos script galifest_script.sql

--- src/main/resources/practicas/galifest/galifest_script.sql
+++ src/main/resources/practicas/galifest/galifest_script.sql
@@ -6,9 +6,10 @@ USE galifest;
 
 CREATE TABLE Evento (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombreDelEvento VARCHAR(100) NOT NULL,
-    fechaHora DATETIME NOT NULL,
+    nombre_del_evento VARCHAR(100),
+    fecha_hora DATETIME,
     ubicacion VARCHAR(150),
-    descripcion TEXT,
-    precioDeCoste DECIMAL(10,2) NOT NULL
+    descripcion VARCHAR(250),
+    precio_de_coste DECIMAL(10,2) NOT NULL
 );
+
